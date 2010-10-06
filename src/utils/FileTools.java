eb6ed5fccf63915c779d05f6396323b09539991d
altdotua@gmail.com <altdotua@gmail.com@eb839ec1-72f8-225f-4814-f0376b86ef47>
2010-10-06 18:34:25 +0000

    Code review & refactoring

--- src/utils/FileTools.java
+++ src/utils/FileTools.java
@@ -8,19 +8,20 @@ import java.io.*;
 import org.apache.log4j.Logger;
 
 import djudge.common.Deployment;
+import djudge.common.Review;
 import djudge.judge.dchecker.RemoteFile;
 
 public class FileTools
 {
 	private static final Logger log = Logger.getLogger(FileTools.class);
-	
+
 	public static boolean deleteFile(String filename)
 	{
 		File f = new File(filename);
 		return f.delete();
 	}
-	
-	public static String getExtension(String file)
+
+	public static String getFileExtension(String file)
 	{
 		File f = new File(file);
 		String name = f.getName();
@@ -29,7 +30,8 @@ public class FileTools
 			return "";
 		return name.substring(k, name.length());
 	}
-	
+
+	@Review
 	public static String getAbsolutePath(String relativePath)
 	{
 		File f = new File(relativePath);
@@ -37,31 +39,32 @@ public class FileTools
 		String res = f.getAbsolutePath().replace("\\.", "").replace("/.", "");
 		return res;
 	}
-	
+
 	public static String getNameOnly(String filePathName)
 	{
 		String name = (new File(filePathName)).getName();
-		String ext = getExtension(name);
+		String ext = getFileExtension(name);
 		return name.substring(0, name.length() - ext.length());
 	}
-	
+
 	public static String getFileName(String filePathName)
 	{
 		return (new File(filePathName)).getName();
 	}
-	
+
 	public static void clearDirectory(String dir)
 	{
 		clearDirectory(dir, new ArrayList<String>());
 	}
-	
+
 	public static String[] getDirectoryListing(String dir)
 	{
 		File file = new File(dir);
-		if (!file.exists() || !file.isDirectory()) return new String[0];
+		if (!file.exists() || !file.isDirectory())
+			return new String[0];
 		return file.list();
 	}
-	
+
 	public static void clearDirectory(String dir, ArrayList<String> excl)
 	{
 		File f1 = new File(dir);
@@ -73,225 +76,208 @@ public class FileTools
 					files[i].delete();
 		}
 	}
-	
+
+	@Review
 	public static String concatPaths(String dir, String file)
 	{
-		return (dir + "/" + file).replace("\\\\", "\\").replace("\\.", "").replace("//", "/");
+		return (dir + "/" + file).replace("\\\\", "\\").replace("\\.", "")
+				.replace("//", "/");
 	}
-	
+
 	/*
-	 * Creates symbolic link from destFilename to srcFilename
-	 * on Linux - creates link
-	 * other platforms - copies file srcFilename to destFilename
-	 * TODO: on Windows NTFS - try to create symlink too
+	 * Creates symbolic link from destFilename to srcFilename on Linux - creates
+	 * link other platforms - copies file srcFilename to destFilename TODO: on
+	 * Windows NTFS - try to create symlink too
 	 */
 	public static boolean createLink(String destFilename, String srcFilename)
 	{
+		// Trying to create link
 		if (Deployment.isOSLinux() && Deployment.useLinks())
 		{
 			File f = new File(destFilename);
 			f.getParentFile().mkdir();
-			
-			String[] command = new String[]{"ln", srcFilename, destFilename};
-			
+
+			String[] command = new String[]
+			{ "ln", srcFilename, destFilename };
+
 			try
 			{
 				Process process = Runtime.getRuntime().exec(command);
 				process.waitFor();
 				return true;
 			}
-			catch (IOException e)
+			catch (Exception ex)
 			{
-				// TODO Auto-generated catch block
-				System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1");
-				e.printStackTrace();
+				log.warn("Link creation failed. Trying to copy file", ex);
 			}
-			catch (InterruptedException e)
-			{
-				// TODO Auto-generated catch block
-				System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1");
-				e.printStackTrace();
-			}
-			return false;
 		}
-		else
-		{
-			return copyFile(destFilename, srcFilename);
-		}
+		// else trying to copy file
+		return copyFile(destFilename, srcFilename);
 	}
-	
-	public static boolean copyFile(String destFilename, String srcFilename, boolean setExecutable)
+
+	public static boolean copyFile(String destFilename, String srcFilename,
+			boolean setExecutable)
 	{
 		try
 		{
 			File scrFile = new File(srcFilename);
 			if (!scrFile.exists())
+			{
 				throw new FileNotFoundException("Cannot find source file");
-			
+			}
+
 			File destFile = new File(destFilename);
 			destFile.getParentFile().mkdirs();
-			
+
 			InputStream in = new FileInputStream(scrFile);
 			OutputStream out = new FileOutputStream(destFile);
-			
+
 			// TODO: review buffer size
 			byte[] buf = new byte[64 * 1024];
 			int len;
-			
+
 			while ((len = in.read(buf)) > 0)
 			{
 				out.write(buf, 0, len);
 			}
-			
+
 			in.close();
 			out.close();
-			
+
 			destFile.setExecutable(setExecutable | scrFile.canExecute());
-			
+
 			return true;
 		}
-		catch(FileNotFoundException ex)
-	    {
-			System.out.println(ex.getMessage() + " in the specified directory.");
-	    }
-	    catch(IOException e)
-	    {
-	    	System.out.println(e.getMessage());
-	    }
+		catch (FileNotFoundException ex)
+		{
+			log.warn(ex.getMessage() + " in the specified directory.", ex);
+		}
+		catch (IOException ex2)
+		{
+			log.error("IOException", ex2);
+		}
 		return false;
 	}
-	
+
 	public static boolean copyFile(String destFilename, String srcFilename)
 	{
 		return copyFile(destFilename, srcFilename, false);
 	}
-	
-	public static void writeFileContent(String file, byte[] content)
+
+	public static void writeFileContent(String filename, byte[] content)
 	{
 		try
 		{
-			//System.out.println(file.toString());
-			File f = new File(file);
+			File f = new File(filename);
 			f.getParentFile().mkdirs();
 			OutputStream out = new FileOutputStream(f);
 			out.write(content, 0, content.length);
 			out.close();
 		}
-	    catch(IOException e)
-	    {
-	    	System.out.println(e.getMessage());      
-	    }
+		catch (IOException ex)
+		{
+			log.error("IOException", ex);
+		}
 	}
-	
-	public static void writeFileContent(String file, String content)
+
+	public static void writeFileContent(String filename, String content)
 	{
 		try
 		{
-			File f = new File(file);
+			File f = new File(filename);
 			f.getParentFile().mkdirs();
 			PrintWriter pw = new PrintWriter(f);
 			pw.println(content);
 			pw.close();
 		}
-	    catch(IOException e)
-	    {
-	    	System.out.println(e.getMessage());      
-	    }
+		catch (IOException ex)
+		{
+			log.error("IOException", ex);
+		}
 	}
-	
+
 	public static byte[] readFileContent(String file)
 	{
 		byte[] res = null;
 		try
 		{
 			File f = new File(file);
-			BufferedInputStream in = new BufferedInputStream(new FileInputStream(f));
-			res = new byte[(int)f.length()];
+			BufferedInputStream in = new BufferedInputStream(
+					new FileInputStream(f));
+			res = new byte[(int) f.length()];
 			in.read(res);
 			in.close();
 		}
-		catch(FileNotFoundException ex)
-	    {
-			System.out.println(ex.getMessage() + " in the specified directory.");
-	    }
-	    catch(IOException e)
-	    {
-	    	System.out.println(e.getMessage());      
-	    }
-	    return res;
+		catch (FileNotFoundException ex)
+		{
+			log.error(ex.getMessage() + " in the specified directory.", ex);
+		}
+		catch (IOException ex)
+		{
+			log.error("IOException", ex);
+		}
+		return res;
 	}
-	
-	public static boolean saveToFile(String s, String filename)
+
+	public static boolean saveToFile(String content, String filename)
 	{
 		try
 		{
 			File f = new File(filename);
 			f.getParentFile().mkdirs();
-            PrintWriter writer = new PrintWriter(new FileOutputStream(filename));
-            writer.print(s);
-            writer.close();
-            return true;
+			PrintWriter writer = new PrintWriter(new FileOutputStream(filename));
+			writer.print(content);
+			writer.close();
+			return true;
 		}
-		catch(FileNotFoundException ex)
-	    {
-			System.out.println(ex.getMessage() + " in the specified directory.");
-	    }
-	    catch(Exception e)
-	    {
-	    	System.out.println("Exception in FileWorks.saveToFile: " + e.getMessage());
-	    	e.printStackTrace();
-	    }
-	    return false;
+		catch (FileNotFoundException ex)
+		{
+			log.error(ex.getMessage() + " in the specified directory.", ex);
+		}
+		return false;
 	}
-	
-	public static void saveToFile(RemoteFile file, String filename)
+
+	public static boolean saveToFile(RemoteFile file, String filename)
 	{
+		// if file content is in memory
 		if (file.fIsPresent)
 		{
-			saveToFile(file.filename, filename);
-			return;
+			return saveToFile(file.filename, filename);
 		}
-		try
-		{
-			// TODO: probably buggy
-			createLink(filename, file.filename);
-			// copyFile(filename, file.filename);
-		}
-	    catch(Exception e)
-	    {
-	    	System.out.println("Exception in FileWorks.saveToFile: " + e.getMessage());
-	    	e.printStackTrace();
-	    }		
+		
+		// file on disk - creating link on it
+		return createLink(filename, file.filename);
 	}
-	
+
 	public static String readFile(String filename)
 	{
 		StringBuilder res = new StringBuilder();
 		try
 		{
-            BufferedReader r = new BufferedReader(new FileReader(new File(filename)));
-            String str = null;
-            while ((str = r.readLine()) != null)
-            {
-            	res.append(str);
-            	res.append('\n');
-            }
-            r.close();
+			BufferedReader r = new BufferedReader(new FileReader(new File(
+					filename)));
+			String str = null;
+			while ((str = r.readLine()) != null)
+			{
+				res.append(str);
+				res.append('\n');
+			}
+			r.close();
 		}
-		catch(FileNotFoundException ex)
-	    {
-			System.out.println(ex.getMessage() + " in the specified directory.");
-	    }
-	    catch(Exception e)
-	    {
-	    	System.out.println(e.getMessage());      
-	    }
-	    return res.toString();
+		catch (FileNotFoundException ex)
+		{
+			log.error(ex.getMessage() + " in the specified directory.", ex);
+		}
+		catch (IOException ex)
+		{
+			log.error("IOException", ex);
+		}
+		return res.toString();
 	}
-	
-	public static void deleteDirectory(String dir)
+
+	public static void deleteDirectory(String directory)
 	{
-		File f = new File(dir);
+		File f = new File(directory);
 		if (f.isDirectory())
 		{
 			File[] files = f.listFiles();
