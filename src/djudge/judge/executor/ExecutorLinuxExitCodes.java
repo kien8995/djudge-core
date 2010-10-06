9c3ab50278852e3b8de09cb0e5638831ecb5ca48
altdotua@gmail.com <altdotua@gmail.com@eb839ec1-72f8-225f-4814-f0376b86ef47>
2010-10-06 19:23:16 +0000

    Working on Issue #15

---
+++ src/djudge/judge/executor/ExecutorLinuxExitCodes.java
@@ -0,0 +1,14 @@
+package djudge.judge.executor;
+
+// taken from runner.cpp
+public interface ExecutorLinuxExitCodes
+{
+	public final static int EXIT_OK	= 50;
+	public final static int EXIT_TLE = 51;
+	public final static int EXIT_MLE = 52;
+	public final static int EXIT_OLE = 53;
+	public final static int EXIT_RE = 54;
+	public final static int EXIT_IE	= 55;
+
+	public final static int EXIT_UNKNOWN = 60;
+}
