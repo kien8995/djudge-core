53a130f7a77bdccf77d0b5cd7ea46f9089d1a25c
altdotua@gmail.com <altdotua@gmail.com@eb839ec1-72f8-225f-4814-f0376b86ef47>
2011-01-23 21:54:43 +0000

    Refctoring work/* internals

--- src/djudge/judge/dcompiler/DistributedFileset.java
+++ src/djudge/judge/dcompiler/DistributedFileset.java
@@ -5,8 +5,12 @@ package djudge.judge.dcompiler;
 import java.io.File;
 import java.util.HashMap;
 
+import org.apache.log4j.Logger;
+
 public class DistributedFileset
 {
+	private static Logger log = Logger.getLogger(DistributedFileset.class);
+	
 	public HashMap<String, DistributedFile> map = new HashMap<String, DistributedFile>();
 	
 	public DistributedFileset()
@@ -16,6 +20,7 @@ public class DistributedFileset
 	
 	public boolean addFile(String filename)
 	{
+		log.trace("Adding file `" + filename + "' to fileset");
 		DistributedFile f = new DistributedFile(filename);
 		map.put(f.filename, f);
 		return true;
@@ -23,6 +28,7 @@ public class DistributedFileset
 	
 	public boolean addFile(String filename, String addAs)
 	{
+		log.trace("Adding file `" + filename + "' to fileset under name `" + addAs + "'");
 		DistributedFile f = new DistributedFile(filename, addAs);
 		map.put(f.filename, f);
 		return true;
@@ -30,11 +36,13 @@ public class DistributedFileset
 	
 	public DistributedFileset(String filename)
 	{
+		log.trace("Creating DistributedFileset with one file:");
 		addFile(filename);
 	}
 	
 	public DistributedFileset(String[] files)
 	{
+		log.trace("Creating DistributedFileset multiple files");
 		for (String file : files)
 		{
 			addFile(file);
@@ -43,6 +51,7 @@ public class DistributedFileset
 	
 	public void readDirectory(String directory)
 	{
+		log.trace("Reading directore `" + directory + "' into fileset");
 		File f = new File(directory);
 		for (File file : f.listFiles())
 		{
@@ -55,15 +64,20 @@ public class DistributedFileset
 	
 	public void unpack(String directory)
 	{
+		log.trace("Unpacking fileset into directory `" + directory + "'");
+		File f = new File(directory);
+		f.mkdirs();
 		String[] files = map.keySet().toArray(new String[0]);
 		for (String file : files)
 		{
+			System.out.println("Saving " + file);
 			map.get(file).saveFile(directory);
 		}
 	}
 	
 	public String getFile()
 	{
+		log.trace("Trying to get default file");
 		return map.keySet().toArray(new String[0])[0];
 	}
 }
